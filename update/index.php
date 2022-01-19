604d368d4e013928acd7bc4913d157fcbdfafe2b
kiran <kiran@bookmundi.com>
2022-01-19 19:48:06 +0545

    update

--- update/index.php
+++ update/index.php
@@ -12,26 +12,21 @@ try {
     //get paarams of PUT and DELETE
     if ($method == 'PUT' || $method == 'DELETE' || $method == 'POST') {
         $params = json_decode(file_get_contents('php://input'), true);
+        if (count(array_intersect(PARAMS, array_keys($params))) < 2) {
+            throw new Exception("Parameters Missing. The required Paramaters are curr and action. All these paraneters are required", 1000);
+        }
+        # ensure no extra params
+        if (count($params) > 3) {
+            throw new Exception("Too many Parameters. The allowed Paramaters are curr and action", 1000);
+        }
+        $currency = $params['curr'];
     } else {
         $method =  strtoupper($_GET['action']);
-
+         $currency = $_GET['curr'];
     }
 
-    // var_dump($method);
-    // die;
-    # ensure PARAM values match the keys in $GET
-    // if (count(array_intersect(PARAMS, array_keys($params))) < 2) {
-    //     throw new Exception("Parameters Missing. The required Paramaters are curr and action. All these paraneters are required", 1000);
-    // }
-    # ensure no extra params
-    // if (count($params) > 3) {
-    //     throw new Exception("Too many Parameters. The allowed Paramaters are curr and action", 1000);
-    // }
     $format =  'xml';
-    $currency = $params['curr'];
-    $action = $method;
-
-
+    
     $currencyHelper = new CurrencyHelper($action, $currency);
     $res = $currencyHelper->updateCurrencyData();
     $response = new Response(200, $res, $format);
