fb32e6d61a693c135b78765b11b354df522b67f9
kiran <kiran@bookmundi.com>
2022-01-19 19:37:50 +0545

    CHANGES

--- update/index.php
+++ update/index.php
@@ -13,19 +13,20 @@ try {
     if ($method == 'PUT' || $method == 'DELETE' || $method == 'POST') {
         $params = json_decode(file_get_contents('php://input'), true);
     } else {
-        
-        $method = $_GET['action'];
+        $method =  strtoupper($_GET['action']);
 
     }
 
+    // var_dump($method);
+    // die;
     # ensure PARAM values match the keys in $GET
-    if (count(array_intersect(PARAMS, array_keys($params))) < 2) {
-        throw new Exception("Parameters Missing. The required Paramaters are curr and action. All these paraneters are required", 1000);
-    }
+    // if (count(array_intersect(PARAMS, array_keys($params))) < 2) {
+    //     throw new Exception("Parameters Missing. The required Paramaters are curr and action. All these paraneters are required", 1000);
+    // }
     # ensure no extra params
-    if (count($params) > 3) {
-        throw new Exception("Too many Parameters. The allowed Paramaters are curr and action", 1000);
-    }
+    // if (count($params) > 3) {
+    //     throw new Exception("Too many Parameters. The allowed Paramaters are curr and action", 1000);
+    // }
     $format =  'xml';
     $currency = $params['curr'];
     $action = $method;
